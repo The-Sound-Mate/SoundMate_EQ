f6ff2f1f5d8b294322abebb24a670c7fcae92d04
Deleloper0922 <semin159157@gmail.com>
2026-05-09 02:27:54 +0900

    V22.0: Bypass regsvr32 with direct registry registration

--- src/main.cpp
+++ src/main.cpp
@@ -7,8 +7,8 @@
 
 using namespace std;
 
-// SoundMate Master Setup v21.1
-// FIXED: Using CORRECT SoundMate GUIDs from RegistryHelper.h
+// SoundMate Direct Setup v22.0
+// FIXED: Manual COM & APO registration to bypass regsvr32 "Module not found" errors.
 
 const char* LOG_PATH = "C:\\Users\\Public\\SoundMate_Total.log";
 const wchar_t* SOUNDMATE_POST_MIX_GUID = L"{E7F4E1C5-F95C-4a7a-8EC8-8AEF24F379A1}";
@@ -24,16 +24,52 @@ void Log(const string& msg) {
     cout << "[SoundMate] " << msg << endl;
 }
 
+// Helper to set a registry string value
+void SetRegString(HKEY hRoot, const wchar_t* subKey, const wchar_t* valueName, const wchar_t* data) {
+    HKEY hKey;
+    if (RegCreateKeyExW(hRoot, subKey, 0, NULL, REG_OPTION_NON_VOLATILE, KEY_WRITE, NULL, &hKey, NULL) == ERROR_SUCCESS) {
+        RegSetValueExW(hKey, valueName, 0, REG_SZ, (BYTE*)data, (DWORD)((wcslen(data) + 1) * sizeof(wchar_t)));
+        RegCloseKey(hKey);
+    }
+}
+
+// Helper to set a registry DWORD value
+void SetRegDWORD(HKEY hRoot, const wchar_t* subKey, const wchar_t* valueName, DWORD data) {
+    HKEY hKey;
+    if (RegCreateKeyExW(hRoot, subKey, 0, NULL, REG_OPTION_NON_VOLATILE, KEY_WRITE, NULL, &hKey, NULL) == ERROR_SUCCESS) {
+        RegSetValueExW(hKey, valueName, 0, REG_DWORD, (BYTE*)&data, sizeof(DWORD));
+        RegCloseKey(hKey);
+    }
+}
+
 bool SetRegistryMultiSZ(HKEY hKey, const wchar_t* valueName, const wchar_t* guid) {
     vector<wchar_t> buffer;
     for (int i = 0; guid[i] != L'\0'; ++i) buffer.push_back(guid[i]);
-    buffer.push_back(L'\0'); // First null
-    buffer.push_back(L'\0'); // Second null
-
-    if (RegSetValueExW(hKey, valueName, 0, REG_MULTI_SZ, (BYTE*)buffer.data(), (DWORD)(buffer.size() * sizeof(wchar_t))) != ERROR_SUCCESS) {
-        return false;
-    }
+    buffer.push_back(L'\0');
+    buffer.push_back(L'\0');
+    if (RegSetValueExW(hKey, valueName, 0, REG_MULTI_SZ, (BYTE*)buffer.data(), (DWORD)(buffer.size() * sizeof(wchar_t))) != ERROR_SUCCESS) return false;
     return true;
+}
+
+void RegisterAPO_Manual(const wchar_t* dllPath) {
+    Log("Performing Manual COM Registration...");
+    wstring clsidKey = L"SOFTWARE\\Classes\\CLSID\\";
+    clsidKey += SOUNDMATE_POST_MIX_GUID;
+    
+    SetRegString(HKEY_LOCAL_MACHINE, clsidKey.c_str(), NULL, L"SoundMateAPO Post-Mix Class");
+    SetRegString(HKEY_LOCAL_MACHINE, (clsidKey + L"\\InprocServer32").c_str(), NULL, dllPath);
+    SetRegString(HKEY_LOCAL_MACHINE, (clsidKey + L"\\InprocServer32").c_str(), L"ThreadingModel", L"Both");
+
+    Log("Performing Manual APO Interface Registration...");
+    wstring apoKey = L"SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Audio\\AudioProcessingObjects\\";
+    apoKey += SOUNDMATE_POST_MIX_GUID;
+    
+    SetRegString(HKEY_LOCAL_MACHINE, apoKey.c_str(), L"FriendlyName", L"SoundMateAPO");
+    SetRegString(HKEY_LOCAL_MACHINE, apoKey.c_str(), L"Copyright", L"Copyright (C) 2026");
+    SetRegDWORD(HKEY_LOCAL_MACHINE, apoKey.c_str(), L"MajorVersion", 1);
+    SetRegDWORD(HKEY_LOCAL_MACHINE, apoKey.c_str(), L"MinorVersion", 0);
+    SetRegDWORD(HKEY_LOCAL_MACHINE, apoKey.c_str(), L"Flags", 0x0000000d); // Frames match, Bits match, In-place
+    SetRegString(HKEY_LOCAL_MACHINE, apoKey.c_str(), L"APOInterface0", L"{f86444da-6a8b-4a4b-97c2-9e9000305886}"); // IAudioProcessingObject
 }
 
 void ProcessDevice(const wchar_t* deviceKeyPath, const wchar_t* apoGuid) {
@@ -43,42 +79,30 @@ void ProcessDevice(const wchar_t* deviceKeyPath, const wchar_t* apoGuid) {
     fullPath += L"\\FxProperties";
 
     if (RegOpenKeyExW(HKEY_LOCAL_MACHINE, fullPath.c_str(), 0, KEY_SET_VALUE | KEY_QUERY_VALUE, &hKey) == ERROR_SUCCESS) {
-        // Inject SoundMate GUID into all slots
         SetRegistryMultiSZ(hKey, L"{d04e05a6-594b-4fb6-a80d-01af5eedf162},5", apoGuid);
         SetRegistryMultiSZ(hKey, L"{d04e05a6-594b-4fb6-a80d-01af5eedf162},6", apoGuid);
         SetRegistryMultiSZ(hKey, L"{d04e05a6-594b-4fb6-a80d-01af5eedf162},7", apoGuid);
-        
         DWORD val = 0;
-        RegSetValueExW(hKey, L"{d04e05a6-594b-4fb6-a80d-01af5eedf162},15", 0, REG_DWORD, (BYTE*)&val, sizeof(DWORD)); // Offload = 0
+        RegSetValueExW(hKey, L"{d04e05a6-594b-4fb6-a80d-01af5eedf162},15", 0, REG_DWORD, (BYTE*)&val, sizeof(DWORD));
         val = 1;
-        RegSetValueExW(hKey, L"{d04e05a6-594b-4fb6-a80d-01af5eedf162},1", 0, REG_DWORD, (BYTE*)&val, sizeof(DWORD));  // Enable = 1
-        
+        RegSetValueExW(hKey, L"{d04e05a6-594b-4fb6-a80d-01af5eedf162},1", 0, REG_DWORD, (BYTE*)&val, sizeof(DWORD));
         RegCloseKey(hKey);
     }
 }
 
 int main() {
-    Log("--- Setup v21.1 Execution Start (GUID Fix) ---");
-
+    Log("--- Setup v22.0 Direct Execution Start ---");
     const wchar_t* dllSource = L"engine\\SoundMate_APO\\build\\SoundMate_APO.dll";
     const wchar_t* dllDest = L"C:\\Program Files\\SoundMate Equalizer\\SoundMate_APO.dll";
 
-    // 1. Copy DLL
-    if (CopyFileW(dllSource, dllDest, FALSE)) {
-        Log("DLL Copied successfully.");
-    } else {
-        Log("DLL Copy status: " + to_string(GetLastError()));
-    }
+    CreateDirectoryW(L"C:\\Program Files\\SoundMate Equalizer", NULL);
+    if (CopyFileW(dllSource, dllDest, FALSE)) Log("DLL Copied successfully.");
+    else Log("DLL Copy Status: " + to_string(GetLastError()));
 
-    // 2. Register COM
-    Log("Registering COM server...");
-    wstring regCmd = L"regsvr32.exe /s \"";
-    regCmd += dllDest;
-    regCmd += L"\"";
-    system(string(regCmd.begin(), regCmd.end()).c_str());
+    // MANUAL REGISTRATION - No regsvr32!
+    RegisterAPO_Manual(dllDest);
 
-    // 3. Inject Devices with NEW GUID
-    Log("Injecting NEW GUID into devices: " + string(SOUNDMATE_POST_MIX_GUID, SOUNDMATE_POST_MIX_GUID + wcslen(SOUNDMATE_POST_MIX_GUID)));
+    Log("Injecting GUID into audio devices...");
     HKEY hRootKey;
     if (RegOpenKeyExW(HKEY_LOCAL_MACHINE, L"SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\MMDevices\\Audio\\Render", 0, KEY_ENUMERATE_SUB_KEYS, &hRootKey) == ERROR_SUCCESS) {
         wchar_t subKeyName[256];
@@ -89,11 +113,10 @@ int main() {
         RegCloseKey(hRootKey);
     }
 
-    // 4. Restart Audio Service
-    Log("Restarting Audio Service...");
+    Log("Restarting Windows Audio Service...");
     system("net stop AudioEndpointBuilder /y");
     system("net start Audiosrv");
 
-    Log("--- Setup v21.1 Execution End ---");
+    Log("--- Setup v22.0 Direct Execution End ---");
     return 0;
 }
